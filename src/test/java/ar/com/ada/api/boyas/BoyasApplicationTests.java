68c424d88b1c4c0b52fc0ad34f63f8ce2230ee41
sami1793 <sami_17_94@hotmail.com>
2021-08-25 19:13:25 -0300

    creo repo para challenge API boyas

---
+++ src/test/java/ar/com/ada/api/boyas/BoyasApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.ada.api.boyas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoyasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
